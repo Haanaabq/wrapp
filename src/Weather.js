92c1c8ba0a12630034392bbde2f29928cb966d7e
haanaabagheri <143870469+haanaabagheri@users.noreply.github.com>
2023-11-05 10:10:45 +0330

    new change

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+    function handdleresp(response) {
+        alert(`the weather in ${response.data.name} is ${response.data.main.temp}`);
+    }
+    let api = `https://api.openweathermap.org/data/2.5/direct?q=${props.city}&units=metric&appid=72bb9dab46b9ec3d65f423c63f27a9b8`;
+
+    axios.get(api).then(handdleresp);
+
+    return <h2>ddd</h2>;
+
+}
